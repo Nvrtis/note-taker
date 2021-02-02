373553e2597578fa946df6276f31c451427e756a
Nicholas Vrtis <nvav91@gmail.com>
2021-02-02 19:27:04 +0100

    adding routes

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const http = require('http')
 const fs = require('fs')
-const PORT = 8080
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8080
+const path = require('path')
 
-const handleRequest = (request, response) => {
-  response.writeHead(200, { "Content-Type": 'text/html' })
-  
-}
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-const server = http.createServer(handleRequest)
-server.listen(PORT, () => {
-  console.log('Server listening on http://localhost:' + PORT)
+
+// * HTML ROUTES
+app.get('*', (req, res) => {
+  res.sendFile( path.join(__dirname + '/public/index.html'))
 })
+
+app.get('/notes', (req, res) => {
+  res.sendFile( path.join(__dirname + '/public/notes.html'))
+})
+
+//  * API ROUTES
+app.get('/api/notes', (req, res) => {
+  res.json(__dirname + `/db/db.json`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`)
+})
+
+
+
+// // add new characters
+// app.post('/api/notes/add', (req, res) => {
+//   const newCharacter = req.body
+//   newCharacter.routeName = newCharacter.name.replace(/ /g, '').toLowerCase()
+//   characters.push(newCharacter)
+//   res.status(200).send()
+// })
